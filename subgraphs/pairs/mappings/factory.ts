09c09a9e4a2a1e9a2c79eb39399cf5aa4ec6a290
christian <101934549+tovaswap1@users.noreply.github.com>
2022-04-27 11:16:35 -0500

    Cambios

--- subgraphs/pairs/mappings/factory.ts
+++ subgraphs/pairs/mappings/factory.ts
@@ -5,7 +5,7 @@ import { PairCreated } from "../generated/Factory/Factory";
 import { fetchDecimals, fetchName, fetchSymbol } from "./utils/erc20";
 
 // Constants
-let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+let FACTORY_ADDRESS = "0x2Adf2E5d40D18A7B6947C92a5879E83eAAaD7c43";
 
 // BigNumber-like references
 let ZERO_BI = BigInt.fromI32(0);
